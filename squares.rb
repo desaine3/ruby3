cdfde98639276344f740d348cbbbf88397f21aff
Tom Close <tom.close@cantab.net>
2013-06-24 19:50:08 +0100

    Changes

--- squares.rb
+++ squares.rb
@@ -1,6 +1,7 @@
-#
+
 # Displays the square numbers from 1 to 25
 #
 def display_squares
 	(1..5).each {|n| puts n**2 }
+	return nil
 end
--- test_triangles.rb
+++ test_triangles.rb
@@ -2,22 +2,20 @@
 require 'minitest/spec'
 require 'minitest/autorun'
 
-# load in double.rb
-require_relative 'squares'
+# load in triangles.rb
+require_relative 'triangles'
 
 # do the test
-describe 'squares' do
-	it "works for 5" do
+describe 'triangles' do
+	it "draws a triangle of size 4" do
 		output = <<EOT
-1
-4
-9
-16
-25
+*
+**
+***
+****
 EOT
 		assert_output(output) do
-			display_squares(5)
+			triangles(4)
 		end
 	end
 end
-
